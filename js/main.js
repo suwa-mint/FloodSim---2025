1b95f4c054febbaf5b90cd2f193e2aefc976b702
Suwa <miituong157lahai@gmail.com>
2025-12-25 03:38:49 +0700

    mamihiep

--- js/main.js
+++ js/main.js
@@ -1,17 +1,1 @@
 // day la file js
-<!DOCTYPE html>
-<html lang="vi">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>FloodSim - Hệ thống cảnh báo ngập lụt</title>
-  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css">
-  <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-  <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-  <script src="js/data.js"></script>
-  <script src="js/main.js"></script> 
-</body>
-</html>
